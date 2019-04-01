e7f52e3b2a68d0418c1f273be00fe2f4fcb69628
thomaswhyyou <thomas.whyyou@gmail.com>
2019-03-31 21:36:29 -0400

    Read default secret key during run time

--- lib/modules/crypto_util.ex
+++ lib/modules/crypto_util.ex
@@ -1,14 +1,12 @@
 defmodule Kitch.CryptoUtil do
-  @secret_key Application.get_env(:kitch, Kitch.CryptoUtil)[:secret_key]
-
   # Largely copypasta from trenpixster/addict: addict/lib/addict/crypto.ex
   # Reference: https://github.com/ueberauth/guardian/issues/152
 
-  def sign(plaintext, key \\ @secret_key) when is_binary(plaintext) do
+  def sign(plaintext, key \\ secret_key()) when is_binary(plaintext) do
     :crypto.hmac(:sha256, key, plaintext) |> Base.url_encode64(padding: false)
   end
 
-  def verify(plaintext, given_sig, key \\ @secret_key) when is_binary(plaintext) do
+  def verify(plaintext, given_sig, key \\ secret_key()) when is_binary(plaintext) do
     base_sig = sign(plaintext, key)
 
     case base_sig == given_sig do
@@ -22,7 +20,7 @@ defmodule Kitch.CryptoUtil do
   # https://www.thegreatcodeadventure.com/elixir-encryption-with-erlang-crypto/
   @aad "AES256GCM"
 
-  def encrypt(plaintext, key \\ @secret_key) when is_binary(plaintext) do
+  def encrypt(plaintext, key \\ secret_key()) when is_binary(plaintext) do
     key = :base64.decode(key)
     iv = :crypto.strong_rand_bytes(16)
 
@@ -31,7 +29,7 @@ defmodule Kitch.CryptoUtil do
     :base64.encode(iv <> tag <> ciphertext)
   end
 
-  def decrypt(ciphertext, key \\ @secret_key) when is_binary(ciphertext) do
+  def decrypt(ciphertext, key \\ secret_key()) when is_binary(ciphertext) do
     key = :base64.decode(key)
     ciphertext = :base64.decode(ciphertext)
 
@@ -42,5 +40,11 @@ defmodule Kitch.CryptoUtil do
 
   def generate_key() do
     :crypto.strong_rand_bytes(16) |> :base64.encode()
+  end
+
+  def secret_key() do
+    # Read during run time (rather than at compile time with module attribute)
+    # so it can be set on the fly
+    Application.get_env(:kitch, Kitch.CryptoUtil)[:secret_key()]
   end
 end
